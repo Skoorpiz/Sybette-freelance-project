0467da74f0d81db055a51e62517d569a116be29d
Skoorpiz <78901299+Skoorpiz@users.noreply.github.com>
2021-05-25 17:51:22 +0200

    Update bdd.php

--- includes/bdd.php
+++ includes/bdd.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $pdo = new PDO("mysql:host=bdd2.adkynet.com;dbname=s2446_sybette;port=3306;charset=utf8", "u2446_cJTMk86qXE", "D.nS1G7paN^qR7YRJRRI=Aeg");
+    $pdo = new PDO("mysql:host=YOURHOST;dbname=YOURDBNAME;port=3306;charset=utf8", "YOURID", "YOURPASSWORD");
 } catch (Exception $ex) {
     echo "<div>Une erreur est survenue : <div><code>$ex</code></div></div>";
     $pdo = null;
